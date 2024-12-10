f12b8cc090b3500932ef23f4bcddf34e42a30bde
DanielSon93 <DanielSon93@github.com>
2024-12-10 23:58:12 +0900

    [fix] 사용되지 않는 React import 구문 삭제

--- src/pages/FindId.tsx
+++ src/pages/FindId.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function FindId() {
   return <div>FindId</div>;
 }
--- src/pages/FindPassword.tsx
+++ src/pages/FindPassword.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function FindPassword() {
   return <div>FindPassword</div>;
 }
