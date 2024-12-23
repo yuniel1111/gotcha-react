ce003e4c67dca3c2203ee96b3d3dac6fd9427b52
DanielSon93 <DanielSon93@github.com>
2024-12-23 22:13:48 +0900

    [design] tailwindcss 공통 스타일 적용

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import '../css/tailwind.css';
+
 function Home() {
   return <div></div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-main': '#143D8B',
+        'brand-sub': '#4280EF',
+        'brand-black': '#353535',
+        'brand-white': '#ffffff',
+        'brand-gray-1': '#D0D0D0',
+        'brand-gray-2': '#B1B1B1',
+        'brand-gray-3': '#AFAFAF',
+        'brand-gray-4': '#696969',
+      },
+    },
   },
   plugins: [],
-}
+};
