973dfafbbac817c2b0535549b4e3c7517f9bae32
DanielSon93 <DanielSon93@github.com>
2024-12-25 21:41:43 +0900

    [feat] Header Mobile 반응형 구현

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,5 +1,5 @@
 import logoImage from '../../assets/gotcha_logo.png';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import {
   BiHome,
   BiBookmark,
@@ -11,9 +11,13 @@ import {
 import { useState } from 'react';
 
 function Header() {
-  const [isActive, setIsActive] = useState<boolean[]>(
-    Array.from({ length: 5 }, () => false),
-  );
+  const [isActive, setIsActive] = useState<boolean[]>(() => {
+    const savedNavActiveState = localStorage.getItem('navActiveState');
+    return savedNavActiveState
+      ? JSON.parse(savedNavActiveState)
+      : [true, false, false, false, false];
+  });
+
   const navigate = useNavigate();
 
   const handleNavMenuClick = (url: string, clickedIdx: number) => {
@@ -23,17 +27,22 @@ function Header() {
     );
 
     setIsActive(newActive);
+    localStorage.setItem('navActiveState', JSON.stringify(newActive));
   };
 
   return (
-    <header className='h-[60px] border border-b-brand-gray-1 flex items-center justify-between px-3'>
-      <Link to='/'>
+    <header className='relative h-[60px] border border-b-brand-gray-1 flex items-center px-3 justify-end md:justify-between md:static'>
+      <div
+        className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer 
+               md:static md:transform-none md:-translate-x-0 md:-translate-y-0'
+        onClick={() => handleNavMenuClick('/', 0)}
+      >
         <img src={logoImage} alt='GOTCHA' className='w-25 h-4' />
-      </Link>
+      </div>
       <nav>
         <ul className='flex gap-1'>
           <li
-            className='cursor-pointer px-2 py-3'
+            className='cursor-pointer px-2 py-3 hidden md:block'
             onClick={() => handleNavMenuClick('/', 0)}
           >
             <BiHome
@@ -41,7 +50,7 @@ function Header() {
             />
           </li>
           <li
-            className='cursor-pointer px-2 py-3'
+            className='cursor-pointer px-2 py-3 hidden md:block'
             onClick={() => handleNavMenuClick('/bookmark', 1)}
           >
             <BiBookmark
@@ -49,7 +58,7 @@ function Header() {
             />
           </li>
           <li
-            className='cursor-pointer px-2 py-3'
+            className='cursor-pointer px-2 py-3 hidden md:block'
             onClick={() => handleNavMenuClick('/note', 2)}
           >
             <BiEditAlt
@@ -57,7 +66,7 @@ function Header() {
             />
           </li>
           <li
-            className='cursor-pointer px-2 py-3'
+            className='cursor-pointer px-2 py-3 hidden md:block'
             onClick={() => handleNavMenuClick('/resume', 3)}
           >
             <BiFileBlank
@@ -68,13 +77,16 @@ function Header() {
             // 유저가 로그인 상태면 마이페이지("/my-page")로 이동하고, 로그아웃 상태면 로그인("/sign-in") 페이지로 이동
           }
           <li
-            className='cursor-pointer px-2 py-3'
+            className='cursor-pointer px-2 py-3 hidden md:block'
             onClick={() => handleNavMenuClick('/sign-in', 4)}
           >
             <BiUser
               className={`w-[20px] h-[20px] ${isActive[4] ? 'text-brand-sub' : 'text-brand-black'}`}
             />
           </li>
+          {
+            // 필터는 페이지 이동이 없기때문에 별도로 관리
+          }
           <li className='cursor-pointer px-2 py-3'>
             <BiSliderAlt className='w-[20px] h-[20px]' />
           </li>
