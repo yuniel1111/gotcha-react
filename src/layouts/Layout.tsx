2a752b570f3ec2800f3fa2a342e89323de4778b8
DanielSon93 <DanielSon93@github.com>
2024-12-30 16:36:19 +0900

    [feat] Outlet 높이 조절로 인한 Filter 상태값 Layout 컴포넌트로 변경

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Layout/Header';
 import BottomNav from '../components/Layout/BottomNav';
+import { useLocation } from 'react-router-dom';
 
 function Layout() {
+  const location = useLocation();
   // Filter 내용이 하나라도 존재하는지 유무 확인 (임시로 정한 초기값인 true 위치에 DB에서 filter가 존재하는지 확인하는 로직 필요)
   const isFilterExists = true;
+  const isFilterOpen =
+    location.pathname === '/' || location.pathname === '/bookmark';
 
   return (
     <>
-      <Header isFilterExists={isFilterExists} />
-      <div className={`${isFilterExists ? 'mt-[120px]' : 'mt-[60px]'}`}>
+      <Header isFilterExists={isFilterExists} isFilterOpen={isFilterOpen} />
+      <div
+        className={`${isFilterExists && isFilterOpen ? 'mt-[120px]' : 'mt-[60px]'}`}
+      >
         <Outlet />
       </div>
       <BottomNav />
