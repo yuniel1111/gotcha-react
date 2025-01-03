206a00c0f9f59ade8c05e0fc823bd608fd11ff13
DanielSon93 <DanielSon93@github.com>
2025-01-03 17:24:52 +0900

    [feat] supabase session을 사용해 유저 로그인 유무 판단

--- src/components/Layout/MenuList.tsx
+++ src/components/Layout/MenuList.tsx
@@ -13,46 +13,46 @@ import {
 import MenuItem from './MenuItem';
 import { useNavigateStore } from '../../stores/useNavigateStore';
 import { v4 as uuid4 } from 'uuid';
-
-const menuItems = [
-  { path: '/', label: 'Home', Icon: BiHome, ActiveIcon: BiSolidHome },
-  {
-    path: '/bookmark',
-    label: 'Bookmark',
-    Icon: BiBookmark,
-    ActiveIcon: BiSolidBookmark,
-  },
-  {
-    path: '/note',
-    label: 'Memo',
-    Icon: BiEditAlt,
-    ActiveIcon: BiSolidEditAlt,
-  },
-  {
-    path: '/resume',
-    label: 'Resume',
-    Icon: BiFileBlank,
-    ActiveIcon: BiSolidFileBlank,
-  },
-  {
-    path: '/sign-in',
-    label: 'My Page',
-    Icon: BiUser,
-    ActiveIcon: BiSolidUser,
-  },
-];
+import { useUserStore } from '../../stores/useUserStore';
 
 function NavMenus() {
   const isActive = useNavigateStore((state) => state.isActive);
+  const isLoggedIn = useUserStore((state) => state.isLoggedIn);
+
+  const menuItems = [
+    { path: '/', label: 'Home', Icon: BiHome, ActiveIcon: BiSolidHome },
+    {
+      path: '/bookmark',
+      label: 'Bookmark',
+      Icon: BiBookmark,
+      ActiveIcon: BiSolidBookmark,
+    },
+    {
+      path: '/note',
+      label: 'Memo',
+      Icon: BiEditAlt,
+      ActiveIcon: BiSolidEditAlt,
+    },
+    {
+      path: '/resume',
+      label: 'Resume',
+      Icon: BiFileBlank,
+      ActiveIcon: BiSolidFileBlank,
+    },
+    {
+      path: isLoggedIn ? '/my-page' : '/sign-in',
+      label: isLoggedIn ? 'My Page' : 'Sign In',
+      Icon: BiUser,
+      ActiveIcon: BiSolidUser,
+    },
+  ];
 
   return (
     <ul className='flex h-[60px] items-center justify-between gap-1 font-bold sm:justify-normal'>
       {menuItems.map((menu, idx) => (
         <MenuItem
           key={uuid4()}
-          // 로그인 상태면 my-page로 이동하고, 로그아웃 상태면 sign-in으로 이동
           path={menu.path}
-          // 로그인 상태면 my-page문구로 표시하고, 로그아웃 상태면 sign-in 문구로 표시
           label={menu.label}
           isActive={isActive[idx]}
           Icon={menu.Icon}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,26 @@
 import { useEffect } from 'react';
 import '../css/tailwind.css';
 import { supabase } from '../api/supabase/supabaseClient';
+import { useUserStore } from '../stores/useUserStore';
 
 function Home() {
-  const checkSupabaseSession = async () => {
-    const { data } = await supabase.auth.getSession();
-  };
+  const { setUserLogin, setUserSession } = useUserStore(
+    (state) => state.actions,
+  );
+  const isLoggedIn = useUserStore((state) => state.isLoggedIn);
 
   useEffect(() => {
-    checkSupabaseSession();
-  }, []);
+    const handleUserLogin = async () => {
+      const { data } = await supabase.auth.getSession();
+
+      if (data?.session && !isLoggedIn) {
+        setUserLogin(true);
+        setUserSession(data.session);
+      }
+    };
+
+    handleUserLogin();
+  });
 
   return <main>Home</main>;
 }
