fc14505a5c5e7de0f3b627b187987c80c07ab4e7
Attainy <attainy00@gmail.com>
2025-02-25 04:09:11 +0900

    [rename] supabase table 파일 이름 변경

--- src/api/supabase/supabaseClient.ts
+++ src/api/supabase/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '../../types/supabaseType';
+import { Database } from '../../types/supabaseTable';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
